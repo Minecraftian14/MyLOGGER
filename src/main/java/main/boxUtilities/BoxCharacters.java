07ef38b160a289e0ef4c366f7591cefa4ea6919f
MCXIV <mcxiv14@gmail.com>
2020-09-02 20:27:46 +0530

    Reinit

---
+++ src/main/java/main/boxUtilities/BoxCharacters.java
@@ -0,0 +1,25 @@
+package main.boxUtilities;
+
+public class BoxCharacters {
+
+    // Corners
+    public static final String TR_DC = "\u2554";
+    public static final String TL_DC = "\u2557";
+    public static final String BR_DC = "\u255A";
+    public static final String BL_DC = "\u255D";
+    public static final String TR_SC = "\u250C";
+    public static final String TL_SC = "\u2510";
+    public static final String BR_SC = "\u2514";
+    public static final String BL_SC = "\u2518";
+
+    // Pillars
+    public static final String DP = "\u2551";
+    public static final String SP = "\u2502";
+
+    // Beams
+    public static final String DB = "\u2550";
+    public static final String SB = "\u2500";
+
+
+
+}
