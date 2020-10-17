2d6b7f358ca922ce3350d0081075d74de03206fa
MCXIV <mcxiv14@gmail.com>
2020-10-17 17:42:46 +0530

    FIN RER

--- DEC/src/main/java/main/decoratedLogger/log/Decorate.java
+++
@@ -1,9 +0,0 @@
-package main.decoratedLogger.log;
-
-import main.utlities.ConsoleColors;
-
-public interface Decorate {
-    String decorate(String s);
-
-    Decorate red = s -> ConsoleColors.ANSI.RED + s + ConsoleColors.ANSI.RESET;
-}
